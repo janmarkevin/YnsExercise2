5d1d6b15f6c5f8d4451469b1e2b7006866d1a2ca
JanmarKevin-Espina <janmarkespina.yns@gmail.com>
2021-10-12 12:54:53 +0800

    wordpress -converted

---
+++ seisaku/footer.php
@@ -0,0 +1,200 @@
+<footer class="footer">
+        <div class="footer__top">
+            <div class="footer-nav">
+                <nav class="footer-nav__wrapper">
+                    <ul class="footer-nav__list">
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="Top">Top</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="制作実績">制作実績</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="制作料金プラン">制作料金プラン</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="ホームページ制作">ホームページ制作</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="ホームページ制作の流れ">ホームページ制作の流れ</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="ホームページっていくらで作れるの？">ホームページっていくらで作れるの？</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="ブランディングしたい！">ブランディングしたい！</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="採用したい！">採用したい！</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="他社と差別化して集客したい！">他社と差別化して集客したい！</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="スタッフ">スタッフ</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="よくある質問">よくある質問</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="代表メッセージ">代表メッセージ</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="会社概要">会社概要</a>
+                            </div>
+                        </li>
+                        <li class="footer-nav__item">
+                            <div class="footer-nav__item-wrapper">
+                                <a href="#" class="footer-nav__links" title="プライバシーポリシー">プライバシーポリシー</a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="footer__wrapper">
+                <div class="footer__logo-links">
+                    <a href="#" class="logo__img-wrapper" title="Logo">
+                        <img src="<?=get_template_directory_uri() ?>/img/logo.png" alt="logo" class="logo__img logo__img--footer">
+                    </a>
+                    <p class="logo__text logo__text--footer">
+                        株式会社アップワード 
+                        <br>
+                        〒110-0015 東京都台東区東上野3-13-2 AYビル2F
+                    </p>
+                </div>
+                <div class="footer__links">
+                    <div class="footer__links-wrapper">
+                        <div class="footer__table">
+                            <table class="table">
+                                <thead class="table__header">
+                                    <tr class="table__row">
+                                        <th class="table__head">
+                                            <a href="#" title="Top" class="table__links--top">Top</a>
+                                        </th>
+                                        <th class="table__head">目的で選ぶ</th>
+                                        <th class="table__head">料金プラン</th>
+                                        <th class="table__head">サポート体制</th>
+                                        <th class="table__head">企業概要</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="table__body">
+                                    <tr class="table__row">
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="ホームページ">ホームページ</a> 
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="ホームページ料金">ホームページ料金</a> 
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="制作の流れ">制作の流れ</a> 
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="会社概要">会社概要</a> 
+                                        </td>
+                                    </tr>
+                                    <tr class="table__row">
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="スマートフォン対応">スマートフォン対応</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="スマートフォン対応料金">スマートフォン対応料金</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="無料診断">無料診断</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="トップメッセージ">トップメッセージ</a>
+                                        </td>
+                                    </tr>
+                                    <tr class="table__row">
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="レスポンシブ対応">レスポンシブ対応</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="レスポンシブ対応料金">レスポンシブ対応料金</a> 
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="よくある質問">よくある質問</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="スタッフ紹介">スタッフ紹介</a>
+                                        </td>
+                                    </tr>
+                                    <tr class="table__row">
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="ランディンクページ">ランディンクページ</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="ランディンクページ料金">ランディンクページ料金</a>
+                                        </td>
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">&nbsp;</td>
+                                    </tr>
+                                    <tr class="table__row">
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="パララックスデザイン">パララックスデザイン</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="パララックスデザイン料金">パララックスデザイン料金</a>
+                                        </td>
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="サイトマップ">サイトマップ</a>
+                                        </td>
+                                    </tr>
+                                    <tr class="table__row">
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="ショッピングサイト">ショッピングサイト</a>
+                                        </td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="ショッピングサイト料金">ショッピングサイト料金</a>
+                                        </td>
+                                        <td class="table__data">&nbsp;</td>
+                                        <td class="table__data">
+                                            <a href="#" class="table__links" title="プライバシーポリシー">プライバシーポリシー</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <span class="footer__copywrite">Copyright © UpWard All right reserved.</span>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ seisaku/index.php
@@ -0,0 +1,227 @@
+<?php get_header(); ?>
+<section class="banner">
+    <div class="banner__img-container">
+        <img src="<?=get_template_directory_uri() ?>/img/banner.jpg" alt="Lovely Office" class="banner__img">
+    </div>
+</section>
+<section class="binding-target">
+    <div class="binding-target__wrapper">
+        <div class="binding-target__container">
+            <h2 class="binding-target__title">Binding Target</h2>
+            <ul class="binding-target__list">
+                <li class="binding-target__item">
+                    <div class="binding-target__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/wanda_01.png" alt="Cat thinking" class="binding-target__img">
+                    </div>
+                    <span class="binding-target__text">ホームページって<br>いくらで作れるの？</span>
+                </li>
+                <li class="binding-target__item">
+                    <div class="binding-target__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/wanda_02.png" alt="Cat Glowing" class="binding-target__img">
+                    </div>
+                    <span class="binding-target__text">ブランディングしたい！</span>
+                </li>
+                <li class="binding-target__item">
+                    <div class="binding-target__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/wanda_03.png" alt="Cat reading Documents" class="binding-target__img">
+                    </div>
+                    <span class="binding-target__text">採用したい！</span>
+                </li>
+                <li class="binding-target__item">
+                    <div class="binding-target__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/wanda_04.png" alt="Cat with a flag" class="binding-target__img">
+                    </div>
+                    <span class="binding-target__text">他社と別化して<br>集害したい！</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<section class="goal">
+    <div class="goal__wrapper">
+        <h2 class="goal__title">Goal</h2>
+        <p class="goal__text">ホームページの最大活用はゴール設定から始まります。<br> 企画からUI・UX設計、動画撮影から制作まで全て社内で構築し、クライアント様の要望の実現をお手伝いします。</p>
+    </div>
+</section>
+<section class="work">
+    <div class="work__wrapper">
+        <h2 class="work__title">Works</h2>
+        <div class="work__list-wrapper">
+            <ul class="work__list">
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/chutoku.png" alt="Chutoku Website" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">中特グループ 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/takumi.png" alt="Website for realstate" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">株式会社たくみ 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/work1.png" alt="Factory Website" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">株式会社富洋組 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/noge.png" alt="Noge Website" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">野毛手形 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/sanri.png" alt="Sanri Website" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">株式会社サンリ 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/mirai.png" alt="Mirai Website" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">COOP 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/shimotsu.png" alt="shimotsu Website" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">シモツトランスポート株式会社 様</span>
+                    </div>
+                </li>
+                <li class="work__item">
+                    <div class="work__img-wrapper">
+                        <img src="<?=get_template_directory_uri() ?>/img/aeonsquare.png" alt="Website Coordinate" class="work__img">
+                    </div>
+                    <div class="work__overlay">
+                        <span class="work__item-title">AEON RETAIL 様</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="work__btn">
+            <div class="work__btn-wrapper">
+                <button class="btn btn--show-more">
+                    <span class="btn__text btn__text--work">もっと見る</span>
+                    <img src="<?=get_template_directory_uri() ?>/img/ArrowIcon.png" alt="Arrow Icon" class="btn__icon--show-more">
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="news">
+    <div class="news__wrapper">
+        <h2 class="news__title">News</h2>
+        <ul class="news__list">
+            <li class="news__item">
+                <div class="news__img-wrapper">
+                    <img src="<?=get_template_directory_uri() ?>/img/ryoji-iwata.jpg" alt="Shibuya crossing" class="news__img">
+                </div>
+                <div class="news__content">
+                    <div class="news__header">
+                        <span class="news__date">2018.00/00</span>
+                        <span class="news__category news__category--add-width">web制作</span>
+                        <div class="news__badge">
+                            <span class="news__badge-text">
+                                New!
+                            </span>
+                        </div>
+                    </div>
+                    <p class="news__text">テキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                </div>
+            </li>
+            <li class="news__item">
+                <div class="news__img-wrapper"></div>
+                <div class="news__content">
+                    <div class="news__header">
+                        <span class="news__date">2018.00/00</span>
+                        <span class="news__category news__category--orange">プロモーション</span>
+                        <div class="news__badge">
+                            <span class="news__badge-text">
+                                New!
+                            </span>
+                        </div>
+                    </div>
+                    <p class="news__text">テキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                </div>
+            </li>
+            <li class="news__item">
+                <div class="news__img-wrapper"></div>
+                <div class="news__content">
+                    <div class="news__header">
+                        <span class="news__date">2018.00/00</span>
+                        <span class="news__category news__category--apple-green">company</span>
+                        <div class="news__badge">
+                            <span class="news__badge-text">
+                                New!
+                            </span>
+                        </div>
+                    </div>
+                    <p class="news__text">テキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                </div>
+            </li>
+        </ul>
+        <div class="news__btn">
+            <div class="news__btn-wrapper">
+                <button class="btn btn--show-more">
+                    <span class="btn__text btn__text--news">もっと見る</span>
+                    <img src="<?=get_template_directory_uri() ?>/img/ArrowIcon.png" alt="Arrow Icon" class="btn__icon--show-more">
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="feature">
+    <div class="feature__wrapper">
+        <h2 class="feature__title">Feature</h2>
+        <p class="feature__text">
+            アップワードは創業当初、プロモーションを中心にスタートさせて頂きました。
+            <br>
+            今も多くのお客様のプロモーションをさせて頂いておりますが、
+            <br>
+            お客様から頂いた制作依頼の要望を叶えるために作った制作事業が珠玉つなぎのようにご紹介を頂き、
+            <br>
+            今のアップワードになっております。
+            <br>
+            どんなにきれいなホームページも、成果につながらなければ価値にならない。
+            <br>
+            小さな力でも必ず役に立つ、仕事のため、マーケティングから制作、
+            <br>
+            プロモーションと常に世の中の流れを読み、最新技術に挑戦し続け成果につなげていきます。
+        </p>
+    </div>
+</section>
+<section class="contacts">
+    <div class="contacts__btn-wrapper">
+        <button class="btn btn--contact btn--contact-grey">
+            <div class="btn__details">
+                <span class="btn__title btn__title--contact btn__title--contact-number">03-5826-4652</span>
+                <span class="btn__text btn__text--contact">午前9:00~午後 18:00</span>
+            </div>
+            <img src="<?=get_template_directory_uri() ?>/img/tele-white.png" alt="Telephone" class="btn__contact-icons">
+        </button>
+        <button class="btn btn--contact">
+            <div class="btn__details">
+                <span class="btn__text btn__text--contact">メールでお問い合わせ</span>
+                <span class="btn__title btn__title--contact">Mail Contact</span>
+            </div>
+            <img src="<?=get_template_directory_uri() ?>/img/mail-white.png" alt="Mail" class="btn__contact-icons">
+        </button>
+    </div>
+</section>
+<?php get_footer(); ?>
